1ca457c67cc0afea73aee164c201a24f53a08ac1
chuasweechin <sampiochua@gmail.com>
2019-03-13 09:58:33 +0800

    upload answer for exercise

--- morse.js
+++ morse.js
@@ -27,3 +27,64 @@ var alphabet = {
   y: '-.--',
   z: '--..'
 };
+
+var result = "";
+
+var firstWord = "hello world";
+var firstWordArray = firstWord.split("");
+
+var secondWord = "we love javascript";
+var secondWordArray = secondWord.split("");
+
+var firstEncodeWord = "... .- -- ..- . .-..    -- --- .-. ... .    .. -. ...- . -. - . -..    -- --- .-. ... .    -.-. --- -.. .";
+var firstEncodeWordArray = firstEncodeWord.split(" ");
+
+var secondEncodeWord = ".-.-- --- ..- .-.    .. -. ... - .-. ..- -.-. - --- .-. ...    .-.. --- ...- .    -.-- --- ..-";
+var secondEncodeWordArray = secondEncodeWord.split(" ");
+
+var thirdEncodeWord =
+    "- .... .. ...    .. ...    - .... .    .- -. ... .-- . .-.    - ---    - .... .    ..- .-.. - .. -- .- - .    --.- ..- . ... - .. --- -.    --- ..-.    .-.. .. ..-. .    - .... .    ..- -. .. ...- . .-. ... .    .- -. -..    . ...- . .-. -.-- - .... .. -. --. ....";
+
+var thirdEncodeWordArray = thirdEncodeWord.split(" ");
+
+function encodeWithMorseCode (wordArray) {
+    console.log(wordArray);
+
+    for (var i = 0; i < wordArray.length; i++) {
+        for (var code in alphabet) {
+            if (wordArray[i] === code) {
+                result = result + alphabet[code];
+            }
+            else if (wordArray[i] === " ") {
+                result = result + " ";
+                break;
+            }
+        }
+    }
+
+    console.log(result);
+    result = "";
+}
+
+function encodeWithMorseCode (encodedWordArray) {
+    console.log(encodedWordArray);
+
+    for (var i = 0; i < encodedWordArray.length; i++) {
+        for (var code in alphabet) {
+            if (encodedWordArray[i] === alphabet[code]) {
+                result = result + code + " ";
+            }
+        }
+    }
+
+    console.log(result);
+    result = "";
+}
+
+//var userInput = prompt("Input a word to be encoded!");
+encodeWithMorseCode(firstWordArray);
+encodeWithMorseCode(secondWordArray);
+
+encodeWithMorseCode(firstEncodeWordArray);
+encodeWithMorseCode(secondEncodeWordArray);
+encodeWithMorseCode(thirdEncodeWordArray);
